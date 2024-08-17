4ff3869c3998e08b272d5391004a6a6fbdcca7ba
YuryBe <yurybenites35@gmail.com>
2024-08-16 19:42:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function encriptar() {
+    let texto = document.getElementById("ingrese_texto").value;
+    let textoEncriptado = texto
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+    document.getElementById("resultado_texto").value = textoEncriptado;
+}
+
+function desencriptar() {
+    let texto = document.getElementById("ingrese_texto").value;
+    let textoDesencriptado = texto
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+    document.getElementById("resultado_texto").value = textoDesencriptado;
+}
